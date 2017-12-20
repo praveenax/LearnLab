1d8ed151388b4187fe77d4ee4043869e86a17644
Praveen <xav.axp@gmail.com>
2017-12-20 19:28:47 +0530

    Added Ang4 learning project

--- Ang4Intro/my-app/src/app/app.module.ts
+++ Ang4Intro/my-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
