29078f045908082ff4d72bddc6288275c6a5914b
praveen kumar a x <xav.axp@gmail.com>
2018-09-27 09:10:21 +0530

    pushing latest changes

---
+++ StoryGen/src/com/kyrax/NationStory.java
@@ -0,0 +1,16 @@
+package com.kyrax;
+
+public class NationStory {
+
+    public static void main(String[] args) {
+        //create kingdoms
+        //simulate turns
+        //each turn - first card tells u - Good/Neutral/Bad Times - General Bonus
+        //then for each kingdom - a GNB is raised.
+        //the powers are recallibrated
+        //ecPower - lets you buy buildings to increase bonuses
+        //defPower - lets you increase the security - people x defPower
+        //offPwer - will let you decrease other kingdom's defPower
+        //culPwer - will add people to kingdom
+    }
+}
---
+++ StoryGen/src/com/kyrax/kingdom/Kingdom.java
@@ -0,0 +1,49 @@
+package com.kyrax.kingdom;
+
+public class Kingdom {
+    String name;
+    int ecPower;
+    int defPower;
+    int offPower;
+    int culPower;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEcPower() {
+        return ecPower;
+    }
+
+    public void setEcPower(int ecPower) {
+        this.ecPower = ecPower;
+    }
+
+    public int getDefPower() {
+        return defPower;
+    }
+
+    public void setDefPower(int defPower) {
+        this.defPower = defPower;
+    }
+
+    public int getOffPower() {
+        return offPower;
+    }
+
+    public void setOffPower(int offPower) {
+        this.offPower = offPower;
+    }
+
+    public int getCulPower() {
+        return culPower;
+    }
+
+    public void setCulPower(int culPower) {
+        this.culPower = culPower;
+    }
+}
