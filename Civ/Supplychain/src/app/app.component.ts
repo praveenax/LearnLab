f980935ccc5ce0bb53512bdd082434f5e437186b
praveen kumar a x <xav.axp@gmail.com>
2020-05-01 20:03:35 +0530

    latest changes

--- Civ/Supplychain/src/app/app.component.ts
+++ Civ/Supplychain/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Supplychain';
+  optionArr = ["Extracter","Transporter","Processor","Refinery"]
+  
+  ngOnInit(){
+  
+  }
 }
